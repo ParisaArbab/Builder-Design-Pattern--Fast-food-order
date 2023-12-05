9f4899922fd5cc5c4b60b97ae69cc71b64e0d33c
Parisa Arbab <121276051+ParisaArbab@users.noreply.github.com>
2023-12-05 04:15:10 -0500

    Add files via upload

---
+++ Builder.java
@@ -0,0 +1,10 @@
+package org.finalExam;
+
+public interface Builder {
+	public void buildBurger();
+	public void buildDrink();
+	public void buildSide();
+	public void buildToy();
+	public FastFoodOrder getResult();
+}
+
